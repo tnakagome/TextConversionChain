52b028e5e2d55da4af4e58eb79a1f74f3ca10cd9
Nakagome Tomoyuki <nakagome@fa2.so-net.ne.jp>
2024-02-29 13:43:52 +0900

    Adding Base64 to Shift_JIS/EUC-JP decoders.

---
+++ src/main/java/nx/domain/tcc/converters/base64/decoders/EUC_JP.java
@@ -0,0 +1,9 @@
+package nx.domain.tcc.converters.base64.decoders;
+
+import nx.domain.tcc.Encoding;
+
+public class EUC_JP extends AbstractDecoder {
+    public EUC_JP() {
+        super(Encoding.EUC_JP);
+    }
+}
---
+++ src/main/java/nx/domain/tcc/converters/base64/decoders/Shift_JIS.java
@@ -0,0 +1,9 @@
+package nx.domain.tcc.converters.base64.decoders;
+
+import nx.domain.tcc.Encoding;
+
+public class Shift_JIS extends AbstractDecoder {
+    public Shift_JIS() {
+        super(Encoding.SJIS);
+    }
+}
