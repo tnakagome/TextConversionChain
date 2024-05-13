87b6e7ab1884f9deb9a80364f61f83ce58284621
Nakagome Tomoyuki <nakagome@fa2.so-net.ne.jp>
2024-05-13 15:36:12 +0900

    Adding modified UTF7 converters

---
+++ src/main/java/nx/domain/tcc/converters/imap/ModifiedUtf7Decoder.java
@@ -0,0 +1,17 @@
+package nx.domain.tcc.converters.imap;
+
+import org.apache.james.imap.api.display.CharsetUtil;
+
+import nx.domain.tcc.AbstractConverter;
+
+public class ModifiedUtf7Decoder extends AbstractConverter {
+    public ModifiedUtf7Decoder() {
+        super("ModifiedUtf7Decoder", "Modified UTF-7 (IMAP) → text");
+        this.shortHelp = "Decode modified UTF-7 (IMAP) string to text";
+    }
+
+    @Override
+    public String convert(String source) {
+        return CharsetUtil.decodeModifiedUTF7(source);
+    }
+}
---
+++ src/main/java/nx/domain/tcc/converters/imap/ModifiedUtf7Encoder.java
@@ -0,0 +1,17 @@
+package nx.domain.tcc.converters.imap;
+
+import org.apache.james.imap.api.display.CharsetUtil;
+
+import nx.domain.tcc.AbstractConverter;
+
+public class ModifiedUtf7Encoder extends AbstractConverter {
+    public ModifiedUtf7Encoder() {
+        super("ModifiedUtf7Encoder", "text → Modified UTF-7 (IMAP)");
+        this.shortHelp = "Encode text to modified UTF-7 format (IMAP)";
+    }
+
+    @Override
+    public String convert(String source) {
+        return CharsetUtil.encodeModifiedUTF7(source);
+    }
+}
